d6b63899add780a3c74cbeaa87b70c21a005b63c
Abby Wheelis <abby.wheelis@centre.edu>
2019-11-02 14:07:16 -0400

    at least I tried

--- javascript.js
+++ javascript.js
@@ -5,6 +5,10 @@ penguinPromise.then(function(penguins)
     console.log("works", penguins);
     
     console.log(penguins.map(getQuizDay));
+    
+    //drawSampleGraph(penguins);
+    
+    //setupSVG(penguins);;
 },
 function(error)                  
 {
@@ -27,15 +31,68 @@ var getQuizesOne = function(penguins)
 
 
 var ys = [7, 9, 3, 6, 7, 5, 6, 7, 5, 7, 4, 6, 5, 7, 5, 6, 9, 6, 5, 8, 5, 6, 9]
-//var ys = d3.range(23)
 
-var points= ys.map(function(y,i)
+/*var ys = function(data)
+    {
+        return data.quizes.map(getGrade = function(grade)
+                                    {
+                                        return quiz.grade
+
+                                    })
+    }*/
+
+
+
+
+//begin attempt to plot some pointses
+var screen = {width: 800, height: 500}
+
+var setupSVG = function(points)
+{
+    d3.select("svg")
+    .attr("width", screen.width)
+    .attr("height", screen.height)
+    
+    var xscale=d3.scaleLinear()
+    xscale.domain([0,d3.max(points, function(p)
+                           {return p.x})])
+    xscale.range([0,screen.width])
+    
+    var yscale=d3.scaleLinear()
+    yscale.domain([0, d3.max(points, function(p)
+                            {return p.y})])
+    yscale.range([0,screen.height])
+    
+    //drawSampleGraph(points,xscale,yscale)
+}
+
+var drawSampleGraph = function(points, xscale, yscale)
 {
     
-    return{
-        x:i,
-        y: y
-    }
-})
+    var points= ys.map(function(y,i)
+        {
 
-console.log("points???", points)
+            return{
+                x:i,
+                y: y
+            }
+        })
+    
+    console.log("points???", points)
+
+    d3.select("svg")
+    .selectAll("circle")
+    .data(points)
+    .enter()
+    .append("circle")
+    .attr("r", 3)
+    .attr("cx", function(point)
+          {return /*xscale*/(point.x)})
+    .attr("cy", function(point)
+          {return /*yscale*/(point.y)})
+    
+}
+
+//setupSVG()
+
+drawSampleGraph()
